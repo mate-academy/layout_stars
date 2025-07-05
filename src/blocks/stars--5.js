2a217c93bb56fc8ea5fbc276a7e21f7388e55687
michelam-pseudo <michelammilian@outlook.com>
2025-07-05 15:46:42 -0300

    Concretização do mockup de um sistema de avaliação segundo as propriedades especificadas

---
+++ src/blocks/stars--5.js
@@ -0,0 +1,15 @@
+document.write(
+    `
+        <div class="stars stars--5">
+            <div class="stars__star"></div>
+
+            <div class="stars__star"></div>
+
+            <div class="stars__star"></div>
+
+            <div class="stars__star"></div>
+
+            <div class="stars__star"></div>
+        </div>
+    `
+)
