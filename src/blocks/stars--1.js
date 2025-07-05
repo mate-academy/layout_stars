d68a98cc0292dceb5d0c8653801538874b31c986
michelam-pseudo <michelammilian@outlook.com>
2025-07-05 15:39:34 -0300

    Diretório report - estrutura da dependência de testes

---
+++ src/blocks/stars--1.js
@@ -0,0 +1,17 @@
+document.write(
+    `
+    
+        <div class="stars stars--1">
+            <div class="stars__star"></div>
+
+            <div class="stars__star"></div>
+
+            <div class="stars__star"></div>
+
+            <div class="stars__star"></div>
+
+            <div class="stars__star"></div>
+        </div>
+    
+    `
+)
---
+++ src/blocks/stars--4.js
@@ -0,0 +1,16 @@
+document.write(
+    `
+        <div class="stars stars--4">
+            <div class="stars__star"></div>
+
+            <div class="stars__star"></div>
+
+            <div class="stars__star"></div>
+
+            <div class="stars__star"></div>
+
+            <div class="stars__star"></div>
+        </div>
+
+    `
+)
